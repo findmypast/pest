32d0ed90bc0810223802f8248423535d66aa75d7
Richard K Miller <richardkmiller@gmail.com>
2014-08-28 22:06:25 -0600

    change the magic number of 3 retries to a class variable that can be set by the user

--- PersistentPest.php
+++ PersistentPest.php
@@ -2,10 +2,13 @@
 
 class PersistentPest extends Pest
 {
+
+	public $retries = 3;
+
 	public function get($url, $data = array(), $headers = array())
 	{
 		$success = false;
-		for ($i = 0 ; $i < 3 && !$success; $i++) {
+		for ($i = 0 ; $i < $this->retries && !$success; $i++) {
 			try {
 				$result = parent::get($url, $data, $headers);
 				$success = true;
@@ -23,7 +26,7 @@ class PersistentPest extends Pest
 	public function put($url, $data, $headers = array())
 	{
 		$success = false;
-		for ($i = 0 ; $i < 3 && !$success; $i++) {
+		for ($i = 0 ; $i < $this->retries && !$success; $i++) {
 			try {
 				$result = parent::put($url, $data, $headers);
 				$success = true;
@@ -41,7 +44,7 @@ class PersistentPest extends Pest
 	public function post($url, $data, $headers = array())
 	{
 		$success = false;
-		for ($i = 0 ; $i < 3 && !$success; $i++) {
+		for ($i = 0 ; $i < $this->retries && !$success; $i++) {
 			try {
 				$result = parent::post($url, $data, $headers);
 				$success = true;
